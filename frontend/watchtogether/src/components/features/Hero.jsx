73679762856137c632f4bfab293a1ffe7d3796d4
NguyenBaoHoan <nguyenbaohoan922004@gmail.com>
2025-09-23 16:56:22 +0700

    [ADD] module create room

--- frontend/watchtogether/src/components/features/Hero.jsx
+++ frontend/watchtogether/src/components/features/Hero.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Youtube, Twitch, Video, Film, Cloud } from 'lucide-react';
 
-const Hero = () => {
+const Hero = ({ onCreateRoomClick, isLoading}) => {
   return (
     <section className="min-h-screen flex items-center justify-center text-white relative px-4">
       <div className="z-10 flex flex-col items-center bg-blue-950/60 bg-opacity-30 backdrop-blur-md p-8 md:p-12 rounded-xl shadow-lg">
         <h1 className="text-5xl md:text-7xl font-bold mb-6">
           explore together.
         </h1>
-        <button className="bg-yellow-400 text-black font-bold py-3 px-8 rounded-lg text-lg hover:bg-yellow-300 transition-transform hover:scale-105">
-          Create your room
+        {/* USE PROPS HERE */}
+        <button 
+          onClick = {onCreateRoomClick}
+          disabled={isLoading}
+          className="bg-yellow-400 text-black font-bold py-3 px-8 rounded-lg text-lg hover:bg-yellow-300 transition-transform hover:scale-105">
+          {isLoading ? 'Creating...' : 'Create Room'}
         </button>
         <p className="mt-3 text-gray-300">(No user account required)</p>
         
--- frontend/watchtogether/src/pages/HomePage.jsx
+++ frontend/watchtogether/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { useRoom } from '../hooks/useRoom'; // 1. Import hook để lấy logic
 import React from 'react';
 import Header from '../components/layout/Header.jsx';
 import Hero from '../components/features/Hero.jsx';
@@ -5,11 +6,12 @@ import About from '../components/features/About.jsx';
 import Footer from '../components/layout/Footer.jsx';
 
 const HomePage = () => {
+  const { createRoom, isLoading } = useRoom(); // 2. Sử dụng hook để lấy hàm và trạng thái
   return (
     <>
       <Header />
       <main>
-        <Hero />
+        <Hero onCreateRoomClick={createRoom} isLoading={isLoading} />
         <About />
       </main>
       <Footer />
