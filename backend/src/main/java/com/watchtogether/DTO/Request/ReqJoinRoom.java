539340dc9530d5aec7634785dab7505a587d4927
NguyenBaoHoan <nguyenbaohoan922004@gmail.com>
2026-02-24 14:50:53 +0700

    Update ReqJoinRoom.java
    dong

--- backend/src/main/java/com/watchtogether/DTO/Request/ReqJoinRoom.java
+++ backend/src/main/java/com/watchtogether/DTO/Request/ReqJoinRoom.java
@@ -1,12 +1,12 @@
-package com.watchtogether.DTO.Request;
+// package com.watchtogether.DTO.Request;
 
-import lombok.Builder;
-import lombok.Data;
+// import lombok.Builder;
+// import lombok.Data;
 
-@Data
-@Builder
-public class ReqJoinRoom {
-    private String displayName;
-    private String inviteCode;
-    private String role;
-}
+// @Data
+// @Builder
+// public class ReqJoinRoom {
+//     private String displayName;
+//     private String inviteCode;
+//     private String role;
+// }
